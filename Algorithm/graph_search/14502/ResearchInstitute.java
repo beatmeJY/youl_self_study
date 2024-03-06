291e09df990d0604429e8d519f1f60cd8fcccc34
beatmejy <beatmejy@naver.com>
2024-03-06 15:26:57 +0900

    자동 학습 커밋 2024-03-06 15:26:57

--- Algorithm/graph_search/14502/ResearchInstitute.java
+++ Algorithm/graph_search/14502/ResearchInstitute.java
@@ -13,7 +13,7 @@ public class ResearchInstitute {
     }
 
     private static int[][] inputValue() {
-        int[][] results = new int[8][8];
+        int[][] results = new int[9][8];
         try (InputStreamReader in = new InputStreamReader(System.in);
              BufferedReader buffer = new BufferedReader(in))
         {
