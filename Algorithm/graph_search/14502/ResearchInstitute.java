f34dc0dc2a491834dcd4443806cdbba1ebea99c5
beatmejy <beatmejy@naver.com>
2024-03-06 15:48:57 +0900

    자동 학습 커밋 2024-03-06 15:48:57

--- Algorithm/graph_search/14502/ResearchInstitute.java
+++ Algorithm/graph_search/14502/ResearchInstitute.java
@@ -18,7 +18,7 @@ public class ResearchInstitute {
         int result = 0;
         for (int i = 0; i < arrays[0][0]; i++) {
             for (int j = 0; j < arrays[0][1]; j++) {
-
+                cre
             }
         }
         return result;
