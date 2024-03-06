a89886929711f3a20db2989932e90f4b2f241aa9
beatmejy <beatmejy@naver.com>
2024-03-06 15:11:54 +0900

    자동 학습 커밋 2024-03-06 15:11:54

--- Algorithm/graph_search/14502/ResearchInstitute.java
+++ Algorithm/graph_search/14502/ResearchInstitute.java
@@ -5,22 +5,28 @@ public class ResearchInstitute {
 
 
     public static void main(String[] args) {
+        
         inputValue();
     }
 
 
 
 
-    private static int inputValue() {
+    private static int[][] inputValue() {
         try (InputStreamReader in = new InputStreamReader(System.in);
              BufferedReader buffer = new BufferedReader(in))
         {
+            int[][] results = new int[0][];
             String line;
+            int count = 0;
             while ((line = buffer.readLine()) != null && !line.equals(""))
             {
-                String[] tokens = line.split("\\s");
-                return Integer.parseInt(tokens[0]);
+                String[] stringLine = line.split("\\s");
+                for (int i = 0; i < stringLine.length; i++) {
+                    results[count][i] = Integer.parseInt(stringLine[i]);
+                }
             }
+            return results;
         } catch (Exception e) {
             e.printStackTrace();
         }
