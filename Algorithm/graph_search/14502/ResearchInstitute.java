1f89c40920d2c1632ffa669426292cbfa97fb7bd
beatmejy <beatmejy@naver.com>
2024-03-06 16:48:58 +0900

    자동 학습 커밋 2024-03-06 16:48:58

--- Algorithm/graph_search/14502/ResearchInstitute.java
+++ Algorithm/graph_search/14502/ResearchInstitute.java
@@ -1,9 +1,11 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ResearchInstitute {
 
-
+    List<Node> roomList = new ArrayList<>();
 
     public static void main(String[] args) {
         int[][] arrays = inputValue();
@@ -12,19 +14,8 @@ public class ResearchInstitute {
 //                System.out.println("arrays[" + i + "][" + j + "] = " + arrays[i][j]);
 //            }
 //        }
-        int result = search(arrays);
-        createNode()
+        createNode();
         System.out.println(result);
-    }
-
-    public static int search(int[][] arrays) {
-        int result = 0;
-        for (int i = 0; i < arrays[0][0]; i++) {
-            for (int j = 0; j < arrays[0][1]; j++) {
-                createNode();
-            }
-        }
-        return result;
     }
 
     class Node {
