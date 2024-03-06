61a3e874c750ceda8b3e5fcc0e5f7398f982d717
beatmejy <beatmejy@naver.com>
2024-03-06 15:46:57 +0900

    자동 학습 커밋 2024-03-06 15:46:57

--- Algorithm/graph_search/14502/ResearchInstitute.java
+++ Algorithm/graph_search/14502/ResearchInstitute.java
@@ -5,20 +5,23 @@ public class ResearchInstitute {
 
     public static void main(String[] args) {
         int[][] arrays = inputValue();
-        for (int i = 0; i < arrays.length; i++) {
-            for (int j = 0; j < arrays[i].length; j++) {
-                System.out.println("arrays[" + i + "][" + j + "] = " + arrays[i][j]);
-            }
-        }
-        search(arrays);
+//        for (int i = 0; i < arrays.length; i++) {
+//            for (int j = 0; j < arrays[i].length; j++) {
+//                System.out.println("arrays[" + i + "][" + j + "] = " + arrays[i][j]);
+//            }
+//        }
+        int result = search(arrays);
+        System.out.println(result);
     }
 
-    public static void search(int[][] arrays) {
+    public static int search(int[][] arrays) {
+        int result = 0;
         for (int i = 0; i < arrays[0][0]; i++) {
             for (int j = 0; j < arrays[0][1]; j++) {
 
             }
         }
+        return result;
     }
 
     public static void infection() {
