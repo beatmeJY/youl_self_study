32965f0ae6953bff29a958f34043867a040369e5
beatmejy <beatmejy@naver.com>
2024-05-04 01:21:00 +0900

    자동 학습 커밋 2024-05-04 01:21:00

--- Algorithm/graph_search/순위.java
+++ Algorithm/graph_search/순위.java
@@ -10,34 +10,6 @@ public class 순위 {
     }
 
     public static int solution(int n, int[][] results) {
-        int answer = 0;
-        List<List<Integer>> resultList = new ArrayList<>();
-        int[] ranks = new int[n];
-
-        for (int i = 1; i <= n; i++) {
-            resultList.add(new ArrayList<Integer>());
-            ranks[i - 1] = n;
-        }
-        for (int i = 0; i < results.length; i++) {
-            int win = results[i][0];
-            int lose = results[i][1];
-            resultList.get(win).add(lose);
-            ranks[win-1]--;
-        }
-        Arrays.stream(ranks).sorted();
-        int before = ranks[0];
-        int count = 1;
-        for (int i = 0; i < ranks.length; i++) {
-            if (before != ranks[i] && count == 1 && i == ranks[i]) {
-                answer++;
-            }
-            if (before == ranks[i]) {
-                count++;
-            } else {
-                count = 1;
-            }
-            before = ranks[i];
-        }
-        return answer;
+       
     }
 }
