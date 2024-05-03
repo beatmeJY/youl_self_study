5649073181ccce8be6774b41d4cb6ecc2dd269e2
beatmejy <beatmejy@naver.com>
2024-05-04 00:36:51 +0900

    자동 학습 커밋 2024-05-04 00:36:51

---
+++ Algorithm/graph_search/순위.java
@@ -0,0 +1,15 @@
+public class 순위 {
+
+    public int solution(int n, int[][] results) {
+        int answer = 0;
+        int[] rank = new int[n];
+
+        for (int i = 1; i <= n + 1; i++) {
+            rank[i] = n;
+        }
+
+        
+
+        return answer;
+    }
+}
