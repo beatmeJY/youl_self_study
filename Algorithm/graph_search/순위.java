a6cff9ca8a52e4766bfc999a49f3be0e733a0c3d
beatmejy <beatmejy@naver.com>
2024-05-04 01:12:01 +0900

    자동 학습 커밋 2024-05-04 01:12:01

--- Algorithm/graph_search/순위.java
+++ Algorithm/graph_search/순위.java
@@ -5,7 +5,8 @@ import java.util.List;
 public class 순위 {
 
     public static void main(String[] args) {
-        solution()
+        int solution = solution(5, new int[][]{{4, 3}, {4, 2}, {3, 2}, {1, 2}, {2, 5}});
+        System.out.println("solution = " + solution);
     }
 
     public static int solution(int n, int[][] results) {
