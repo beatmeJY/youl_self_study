72186d1ae7e415dffcc29642f5761ba81d6943c0
beatmejy <beatmejy@naver.com>
2024-05-05 00:04:32 +0900

    자동 학습 커밋 2024-05-05 00:04:32

--- Algorithm/greedy/조이스틱.java
+++ Algorithm/greedy/조이스틱.java
@@ -9,7 +9,7 @@ public class 조이스틱 {
         String al = "ABCDEFGHIJKLNMOPQRSTUVWXYZ";
         length = name.length();
         visit = new int[length];
-        
+
         for (int i = 0; i < name.length(); i++) {
             if ("A" == String.valueOf(name.charAt(i))) {
                 visit[i] = 1;
