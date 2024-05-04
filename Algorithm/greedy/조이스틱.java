9ff5b0147e96473110d856386b2605343fe3eb47
beatmejy <beatmejy@naver.com>
2024-05-04 21:45:32 +0900

    자동 학습 커밋 2024-05-04 21:45:32

--- Algorithm/greedy/조이스틱.java
+++ Algorithm/greedy/조이스틱.java
@@ -1,12 +1,32 @@
 public class 조이스틱 {
+    int[] visit;
+
     public int solution(String name) {
         int answer = 0;
         String al = "ABCDEFGHIJKLNMOPQRSTUVWXYZ";
+        visit = new int[name.length()];
 
         for (int i = 0; i < name.length(); i++) {
-            String word = String.valueOf(name.charAt(i));
-            
+            if ("A" == String.valueOf(name.charAt(i))) {
+                visit[i] = 1;
+            }
+        }
+
+        for (int i = 0; i < name.length(); i++) {
+            if (visit[i] == 1) {
+                findNext(i);
+            }
         }
         return answer;
     }
+
+    private void findNext(int i) {
+        int right = visit[i + 1];
+        int left = visit[i - 1];
+        if (right == left) {
+            findNext();
+        }
+    }
+
+
 }
