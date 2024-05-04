89c201a854f7b1f0f7f553c3dbeb7e26fa27afdb
beatmejy <beatmejy@naver.com>
2024-05-04 21:24:37 +0900

    자동 학습 커밋 2024-05-04 21:24:37

---
+++ Algorithm/greedy/조이스틱.java
@@ -0,0 +1,12 @@
+public class 조이스틱 {
+    public int solution(String name) {
+        int answer = 0;
+        String al = "ABCDEFGHIJKLNMOPQRSTUVWXYZ";
+
+        for (int i = 0; i < name.length(); i++) {
+            String word = String.valueOf(name.charAt(i));
+            
+        }
+        return answer;
+    }
+}
