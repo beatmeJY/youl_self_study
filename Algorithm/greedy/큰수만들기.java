db35fe786db3e7c7d27bab5b74b03cbb3a042581
beatmejy <beatmejy@naver.com>
2024-05-05 17:35:19 +0900

    자동 학습 커밋 2024-05-05 17:35:19

--- Algorithm/greedy/큰수만들기.java
+++ Algorithm/greedy/큰수만들기.java
@@ -1,6 +1,10 @@
 public class 큰수만들기 {
 
-    public String solution(String number, int k) {
+    public static void main(String[] args) {
+        solution("")
+    }
+
+    public static String solution(String number, int k) {
         int count = k;
         int index = 0;
         while (count > 0) {
@@ -21,12 +25,12 @@ public class 큰수만들기 {
                 while (true) {
                     int is = Integer.parseInt(String.valueOf(number.charAt(index)));
                     int next = Integer.parseInt(String.valueOf(number.charAt(index + 1)));
-                    if (is > next) {
-                        index++;
-                    } else if (index == number.length()) {
+                    if (index == number.length() - 1) {
                         number.substring(0, number.length() - 1);
                         count--;
                         break;
+                    }else if (is > next) {
+                        index++;
                     } else {
                         number = number.substring(0, index) + number.substring(index + 1);
                         count--;
