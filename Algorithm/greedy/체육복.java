51ea679ce3af53319c0c9c5ee87e679d7f44af54
beatmejy <beatmejy@naver.com>
2024-05-05 18:56:20 +0900

    자동 학습 커밋 2024-05-05 18:56:20

--- Algorithm/greedy/체육복.java
+++ Algorithm/greedy/체육복.java
@@ -1,6 +1,13 @@
+import java.util.Arrays;
+
 public class 체육복 {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+
+
+
         return answer;
     }
 }
