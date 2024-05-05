5862c905abc88e06a717014a5c80137f6a6f1b43
beatmejy <beatmejy@naver.com>
2024-05-05 17:07:20 +0900

    자동 학습 커밋 2024-05-05 17:07:20

--- Algorithm/greedy/큰수만들기.java
+++ Algorithm/greedy/큰수만들기.java
@@ -1,6 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class 큰수만들기 {
 
     public String solution(String number, int k) {
-        
+        int count = k;
+        List<Integer> deleteList = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            int first = Integer.parseInt(String.valueOf(number.charAt(i)));
+            int second = Integer.parseInt(String.valueOf(number.charAt(i + 1)));
+            if (first < second) {
+                deleteList.add(i);
+            }
+        }
+
+        // deleteList 로 삭제하는 로직
+        return null;
     }
 }
