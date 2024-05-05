f4fd7805637a563220f3028fb2419d7ff3c8ee18
beatmejy <beatmejy@naver.com>
2024-05-05 19:51:20 +0900

    자동 학습 커밋 2024-05-05 19:51:20

--- Algorithm/greedy/구명보트.java
+++ Algorithm/greedy/구명보트.java
@@ -1,4 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class 구명보트 {
 
-    
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        List<Integer> list = Arrays.stream(people).sorted().boxed().collect(Collectors.toList());
+
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = list.size(); j > i; j--) {
+                if (list.get(j) + list.get(i) == limit) {
+                    
+                }
+            }
+        }
+        return answer;
+    }
 }
