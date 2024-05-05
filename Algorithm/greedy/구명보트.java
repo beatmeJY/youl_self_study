73e24e03999488cbccc96adc3ec08b763c0f7e17
beatmejy <beatmejy@naver.com>
2024-05-05 21:08:20 +0900

    자동 학습 커밋 2024-05-05 21:08:20

--- Algorithm/greedy/구명보트.java
+++ Algorithm/greedy/구명보트.java
@@ -2,12 +2,27 @@ import java.util.Arrays;
 
 public class 구명보트 {
 
+    int[] peoples;
+    int limitWeight;
+
     public int solution(int[] people, int limit) {
         int answer = 0;
-        Arrays.stream(people).sorted();
-        people.re
+        limit = limit;
+        peoples = Arrays.stream(people).sorted().toArray();
 
+        for (int i = peoples.length-1; i >= 0 ; i--) {
+           int[] dd = new int[7]{1,2};
+            int[] max = dfs();
+        }
 
         return answer;
+    }
+
+    private int[] dfs(int[] sumArray) {
+        for (int i = index; i >= 0; i--) {
+            if (limitWeight >= people + peoples[i]) {
+
+            }
+        }
     }
 }
