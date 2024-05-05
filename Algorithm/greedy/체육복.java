250cc6978ebba7ed863c089d7c769242c3a75453
beatmejy <beatmejy@naver.com>
2024-05-05 19:24:18 +0900

    자동 학습 커밋 2024-05-05 19:24:18

--- Algorithm/greedy/체육복.java
+++ Algorithm/greedy/체육복.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,10 +12,8 @@ public class 체육복 {
 
     public static int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
-        List<Integer> lostList = Arrays.stream(lost).sorted().mapToObj(Integer::valueOf).collect(Collectors.toList());
-        List<Integer> reserveList = Arrays.stream(lost).sorted().mapToObj(Integer::valueOf).collect(Collectors.toList());
-
-
+        List<Integer> lostList = Arrays.stream(lost).sorted().boxed().collect(Collectors.toList());
+        List<Integer> reserveList = Arrays.stream(lost).sorted().boxed().collect(Collectors.toList());
         for (int i = 0; i < lost.length; i++) {
             lostList.remove((Object)lost[i]);
             reserveList.remove((Object)lost[i]);
