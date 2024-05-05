3c7056628b0c2b326b07493bedf8e9653b33ad21
beatmejy <beatmejy@naver.com>
2024-05-05 22:21:18 +0900

    자동 학습 커밋 2024-05-05 22:21:18

--- Algorithm/dfs_bfs/단어변환.java
+++ Algorithm/dfs_bfs/단어변환.java
@@ -1,7 +1,28 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class 단어변환 {
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
-        
+        boolean[] visit = new boolean[words.length];
+        PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparing(c -> c[0]));
+
+        for (int i = 0; i < words.length; i++) {
+            int count = 0;
+            for (int j = 0; j < begin.length(); j++) {
+                if (begin.charAt(j) != words[i].charAt(j)) {
+                    count++;
+                }
+            }
+            if (count == 1) {
+                queue.add(new int[]{1, i});
+                visit[i] = true;
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            
+        }
 
 
         return answer;
