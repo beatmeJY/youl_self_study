ae16eca20f0425b42b140b423dd6a7219af64253
beatmejy <beatmejy@naver.com>
2024-05-05 20:57:20 +0900

    자동 학습 커밋 2024-05-05 20:57:20

--- Algorithm/greedy/구명보트.java
+++ Algorithm/greedy/구명보트.java
@@ -1,45 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class 구명보트 {
 
-    public int solution(int[] people, int limit) {
-        int answer = 0;
-        Arrays.sort(people);
-
-        while (true) {
-            for (int maxIndex = people.length - 1; maxIndex >= 0; maxIndex--) {
-                if (limit - 40 < people[maxIndex]) {
-                    people[maxIndex] = 0;
-                    answer++;
-                    continue;
-                }
-                int max = people[maxIndex];
-                List<Integer> maxSumList = new ArrayList<>();
-                for (int i = maxIndex - 1; i >= 0; i--) {
-                    List<Integer> sumList = new ArrayList<>();
-                    int sum = people[maxIndex] + people[i];
-                    if (limit >= sum) {
-                        sumList.add(i);
-
-                        if (limit - 40 < sum && max < sum) {
-                            max = sum;
-                            maxSumList = sumList;
-                            continue;
-                        }
-
-
-                        for (int j = i; j >= 0 ; j++) {
-                            if (limit > sum + people[j]) {
-
-                            }
-                        }
-                    }
-                }
-
-            }
-        }
-        return answer;
-    }
+    
 }
