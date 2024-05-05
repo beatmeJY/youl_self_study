15f698d4c4929f360db87633909afc882aa7afb0
beatmejy <beatmejy@naver.com>
2024-05-05 16:43:58 +0900

    자동 학습 커밋 2024-05-05 16:43:58

---
+++ Algorithm/greedy/큰수만들기.java
@@ -0,0 +1,24 @@
+public class 큰수만들기 {
+
+    public String solution(String number, int k) {
+        String answer = "";
+        int count = k;
+        int index = 0;
+        while (count > 0) {
+            if (count > 1) {
+                int max = 0;
+                for (int i = index; i <= index + count; i++) {
+                    max = Integer.max(max, number.charAt(i));
+                }
+                number.substring(index, max);
+            } else {
+
+            }
+        }
+
+
+
+
+        return answer;
+    }
+}
