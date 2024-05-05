90bc6273c2e96c6369fa48c789e802541123ac59
beatmejy <beatmejy@naver.com>
2024-05-05 20:58:20 +0900

    자동 학습 커밋 2024-05-05 20:58:20

--- Algorithm/greedy/구명보트.java
+++ Algorithm/greedy/구명보트.java
@@ -1,4 +1,12 @@
+import java.util.Arrays;
+
 public class 구명보트 {
 
-    
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.stream(people).sorted();
+
+
+        return answer;
+    }
 }
